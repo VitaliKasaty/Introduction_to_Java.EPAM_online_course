0618e44d5db5bd5d60e1682cc91d21e7badb6f6b
Vitali Kasaty <vitalik007007@gmail.com>
2021-11-18 20:54:27 +0300

    edit decomposition7

--- by/epam/java_introduction/algorithmization/Decomposition7.java
+++ by/epam/java_introduction/algorithmization/Decomposition7.java
@@ -12,23 +12,16 @@ public class Decomposition7 {
 		System.out.println("Результат = " + result);
 	}
 
-	public static int getFactorial(int a) {
-
-		int factorial = 1;
-		
-		for (int i = 2; i <= a; i++) {
-			factorial *= i;
-		}
-		return factorial;
-	}
-
+	
 	public static int sumFactorial(int a) {
 
 		int sum = 0;
+		int factorial = 1;
 
 		for (int i = 1; i <= a; i++) {
+			factorial *= i;
 			if (i % 2 == 1) {
-				sum += getFactorial(i);
+				sum += factorial;
 			}
 		}
 		return sum;
